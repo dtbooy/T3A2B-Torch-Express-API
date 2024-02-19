ef571357d0633644e384887699b9f5c8a55d4356
dtbooy <dtbooy@gmail.com>
2024-02-19 18:11:00 +1000

    add /users/:id - GET route

--- routes/user_routes.js
+++ routes/user_routes.js
@@ -10,13 +10,22 @@ userRoutes.get("/", async (req, res) => {
     try {
         res.status(200).send(await User.find().exec())
     } catch (err) {
-        res.status(500).send(err)
+        res.status(500).send({ error: err })
     }
 })
 
 // /users - POST
 
 // /users/:id - GET
+userRoutes.get("/:id", async (req, res) => {
+    res.status(200).send(
+        await User.findById(req.params.id).exec()
+        .catch((err) => { 
+            res.status(404).send({ error: err })
+        })
+    )
+});
+
 
 // /users/:id  - PUT
 
