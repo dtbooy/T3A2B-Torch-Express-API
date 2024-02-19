49e1099331153b4cc26c1fe19b65d0675f9094c0
mini109 <derdriumcateer@gmail.com>
2024-02-20 08:03:56 +1000

    resolve merge

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 import express from 'express'
 
 
-import locationRoutes from './location_routes.js'
-import reservationRoutes from './reservation_routes.js'
 // import cors from 'cors'
 
 const app = express()
@@ -14,8 +12,7 @@ app.use(express.json())
 app.get('/', (req, res) => res.send({ info: 'Torch API' }))
 
 
-app.use('/locations', locationRoutes)
-app.use('/reservations', reservationRoutes)
+
 
 export default app
 
