d1413aae167c9970e1f188d6d6c643430150d926
mini109 <derdriumcateer@gmail.com>
2024-02-20 08:07:10 +1000

    added routers

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 import express from 'express'
-
-
+import locationRoutes from './location_routes.js'
+import reservationRoutes from './reservation_routes.js'
 // import cors from 'cors'
 
 const app = express()
@@ -12,7 +12,8 @@ app.use(express.json())
 app.get('/', (req, res) => res.send({ info: 'Torch API' }))
 
 
-
+app.use('/locations', locationRoutes)
+app.use('/reservations', reservationRoutes)
 
 export default app
 
