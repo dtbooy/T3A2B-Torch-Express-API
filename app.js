92067b32d25b590f016fa5792462411dd002cf29
dtbooy <dtbooy@gmail.com>
2024-02-20 11:25:54 +1000

    Add services routes

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ import locationRoutes from './routes/location_routes.js'
 import reservationRoutes from './routes/reservation_routes.js'
 // import cors from 'cors'
 import userRoutes from './routes/user_routes.js'
+import servicesRoutes from './routes/services_routes.js'
 
 
 const app = express()
@@ -13,11 +14,13 @@ app.use(express.json())
 
 // Attach routers
 app.use("/users", userRoutes)
+app.use('/locations', locationRoutes)
+app.use('/reservations', reservationRoutes)
+app.use('/services', servicesRoutes)
+
 app.get('/', (req, res) => res.send({ info: 'Torch API' }))
 
 
-app.use('/locations', locationRoutes)
-app.use('/reservations', reservationRoutes)
 
 export default app
 
