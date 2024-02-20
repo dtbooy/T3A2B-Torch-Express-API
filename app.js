8406fdc74af3ccda2c37b0452ad3461225c59609
maxhandley97 <maxhandley97@gmail.com>
2024-02-20 14:50:18 +1100

    uncommented cors and added auth.js

--- app.js
+++ app.js
@@ -1,16 +1,15 @@
 import express from 'express'
 import locationRoutes from './routes/location_routes.js'
 import reservationRoutes from './routes/reservation_routes.js'
-// import cors from 'cors'
+import cors from 'cors'
 import userRoutes from './routes/user_routes.js'
-
 import servicesRoutes from './routes/services_routes.js'
 import loginRoutes from './routes/login.js'
 
 
 const app = express()
 
-// app.use(cors())
+app.use(cors())
 
 app.use(express.json())
 
